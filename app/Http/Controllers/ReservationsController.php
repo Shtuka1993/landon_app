77e5922a70356ed051ed63240a47ff768dfa7eee
Yuriy Shtokaylo <Shtokaylo.Yuriy@gmail.com>
2022-11-24 21:43:13 +0200

    Use relationships with Eloquent

--- app/Http/Controllers/ReservationsController.php
+++ app/Http/Controllers/ReservationsController.php
@@ -3,10 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Client as Client;
+use App\Room as Room;
+use App\Reservation as Reservation;
 
 class ReservationsController extends Controller
 {
     public function bookRoom($client_id, $room_id, $date_in, $date_out) {
+        $reservation = new Reservation();
+        $client_instance = new Client();
+        $room_instance = new Room();
+
+        $client = $client_instance->find($client_id);
+        $room = $room_instance->find($room_id);
+
+        $reservation->date_in = $date_in;
+        $reservation->date_out = $date_out;
+
+        $reservation->room()->associate($room);
+        $reservation->client()->associate($client);
+
+        $reservation->save();
+
+        return redirect()->route('clients');
 
         return view('reservations/bookRoom');
     }
