3e9c2313bdd2280a377ceadf95a5af3f68ae0e9e
Yuriy Shtokaylo <Shtokaylo.Yuriy@gmail.com>
2022-11-26 00:14:42 +0200

    Use group for middleware

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,17 @@
 
 Route::get('/', 'ContentsController@home')->name('home');
 
-
-Route::get('/clients', 'ClientController@index')->name('clients')->middleware('auth');
-Route::get('/clients/new', 'ClientController@newClient')->name('new_client');
-Route::post('/clients/new', 'ClientController@newClient')->name('create_client');
-#Route::post('/clients/new', 'ClientController@create')->name('create_client');
-Route::get('/clients/{clients_id}', 'ClientController@show')->name('show_client');
-Route::post('/clients/modify/{clients_id}', 'ClientController@modify')->name('update_client');
-Route::get('reservations/{client_id}', 'RoomsController@checkAvailableRooms')->name('check_room');
-Route::post('reservations/{client_id}', 'RoomsController@checkAvailableRooms')->name('');
-Route::get('/book/room/{client_id}/{room_id}/{date_in}/{date_out}', 'ReservationsController@bookRoom')->name('book_room');
+Route::middleware('auth')->group( function() { 
+    Route::get('/clients', 'ClientController@index')->name('clients');
+    Route::get('/clients/new', 'ClientController@newClient')->name('new_client');
+    Route::post('/clients/new', 'ClientController@newClient')->name('create_client');
+    #Route::post('/clients/new', 'ClientController@create')->name('create_client');
+    Route::get('/clients/{clients_id}', 'ClientController@show')->name('show_client');
+    Route::post('/clients/modify/{clients_id}', 'ClientController@modify')->name('update_client');
+    Route::get('reservations/{client_id}', 'RoomsController@checkAvailableRooms')->name('check_room');
+    Route::post('reservations/{client_id}', 'RoomsController@checkAvailableRooms')->name('');
+    Route::get('/book/room/{client_id}/{room_id}/{date_in}/{date_out}', 'ReservationsController@bookRoom')->name('book_room');
+ } );
 
 Route::get('/about', function () {
     return '<h3>About</h3>';
