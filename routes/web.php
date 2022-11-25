03393717321a809eb30745802dbf9b8e2b658614
Yuriy Shtokaylo <Shtokaylo.Yuriy@gmail.com>
2022-11-25 18:22:48 +0200

    Generated method for generating password

--- routes/web.php
+++ routes/web.php
@@ -65,3 +65,7 @@ Route::get('/facades/decrypt', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get( 'generate/password', function() {
+    return bcrypt('123456789');
+} );
